038f322594448c70356933e5075d6c034eded0b5
Sunjae.Park <sunjae.park.kr@gmail.com>
2017-04-24 10:38:37 +0900

    Initial Commit

---
+++ App Store/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Store
+//
+//  Created by ParkSunJae on 2017. 4. 24..
+//  Copyright © 2017년 heaven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

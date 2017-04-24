f603b3041ae501cf79a728be77e2d9f8ac97243a
Sunjae.Park <sunjae.park.kr@gmail.com>
2017-04-24 13:58:38 +0900

    Implemented UI of App List

---
+++ App Store/AppListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  App Store
+//
+//  Created by ParkSunJae on 2017. 4. 24..
+//  Copyright © 2017년 heaven. All rights reserved.
+//
+
+import UIKit
+
+class AppListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+}
